d3be9239905de62fa3df4be35bacded456d88f70
Graham Ramsey <graham.ramsey@gmail.com>
2020-04-26 08:55:17 +0100

    removed tokio, using std::net etc. instead

--- redfoam/src/main.rs
+++ redfoam/src/main.rs
@@ -1,46 +1,62 @@
 #![warn(rust_2018_idioms)]
 
-use tokio;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+// use tokio;
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+// use tokio::net::TcpListener;
+// use tokio::fs::File;
+
+use std::fs::{File, OpenOptions};
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
 
 use std::env;
 use std::error::Error;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+    let mut file = OpenOptions::new().append(true).open("/tmp/foo.txt").unwrap();
+
+    while match stream.read(&mut buf) {
+        Ok(size) => {
+
+            stream.write(&buf[0..size]).unwrap();
+
+            file.write(&buf[0..size]).unwrap();
+
+            true
+        },
+
+        Err(_) => {
+            println!("Error with tcp stream to {}, (terminating connection)", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+
+            false
+        }
+    } {}
+}
+
+
+fn main() {
     let addr = env::args()
             .nth(1)
             .unwrap_or_else(|| "127.0.0.1:9090".to_string());
 
-    let mut listener = TcpListener::bind(&addr).await?;
+    let listener = TcpListener::bind(&addr).unwrap();
     println!("Listening on: {}", addr);
 
-    loop {
-        let (mut socket, _) = listener.accept().await?;
 
-
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = socket
-                    .read(&mut buf)
-                    .await
-                    .expect("failed to read data from socket");
-
-                if n == 0 {
-                    return;
+    for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    println!("new client! {}", stream.peer_addr().unwrap());
+                    handle_client(stream);
                 }
 
-                socket
-                    .write_all(&buf[0..n])
-                    .await
-                    .expect("failed to write data to socket");
+                Err(e) => {
+                    panic!("connection failed");
+                }
             }
-        });
-
     }
 
 }
