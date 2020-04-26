7353a34d05fd7453b45da5b09498ec6bd12d32f2
Graham Ramsey <graham.ramsey@gmail.com>
2020-04-26 11:52:06 +0100

    added 'starter' thread

--- redfoam/src/main.rs
+++ redfoam/src/main.rs
@@ -9,6 +9,10 @@ use std::fs::{File, OpenOptions};
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
 
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
 use std::env;
 use std::error::Error;
 
@@ -42,21 +46,37 @@ fn main() {
             .nth(1)
             .unwrap_or_else(|| "127.0.0.1:9090".to_string());
 
+    let chan : (mpsc::Sender<TcpStream>, mpsc::Receiver<TcpStream>) = mpsc::channel();
+    let (tx, rx) = chan;
+
+    let thread1 = thread::spawn(move || {
+        let message = rx.recv();
+        match message {
+            Ok(instream) => {
+                handle_client(instream);
+            },
+
+            Err(e) => {
+                println!("  OOOPSS"); 
+            }
+        }
+    });
+
+
     let listener = TcpListener::bind(&addr).unwrap();
     println!("Listening on: {}", addr);
 
 
     for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    println!("new client! {}", stream.peer_addr().unwrap());
-                    handle_client(stream);
-                }
+        match stream {
+            Ok(stream) => {
+                tx.send(stream).unwrap();
+            },
 
-                Err(e) => {
-                    panic!("connection failed");
-                }
+            Err(e) => {
+                panic!("connection failed");
             }
+        }
     }
 
 }
